e57838cebf0cfa0fabf26267eaa54e5a0255fc4b
Marcela Aguirre <aguirremarcela@hotmail.com.ar>
2020-11-24 00:41:46 -0300

    Se modifico una modifico app-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosListComponent } from './cursos-list/cursos-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosListComponent
   ],
   imports: [
     BrowserModule,
