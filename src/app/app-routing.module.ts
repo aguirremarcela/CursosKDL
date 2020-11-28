7b697644985fcffedfad930f9e7ba26ea3a45826
Marcela Aguirre <aguirremarcela@hotmail.com.ar>
2020-11-28 00:17:49 -0300

    configuracion del route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { KdlContactoComponent } from './kdl-contacto/kdl-contacto.component';
+import { KdlCursosComponent } from './kdl-cursos/kdl-cursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', redirectTo:'cursoskdl',pathMatch:'full'
+  },
+  {
+    path:'cursoskdl',component:KdlCursosComponent
+  },
+  {
+    path:'contacto',component:KdlContactoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
